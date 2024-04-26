faa385e6efa742d3717f01d161c874effb39986e
zijiren233 <pyh1670605849@gmail.com>
2024-04-26 13:21:06 +0800

    Opt: run.sh

--- run.sh
+++ run.sh
@@ -2,8 +2,7 @@
 
 set -e
 
-bash ./adguardhome.sh
-bash ./nginx.sh $@
+RAW_ARGS="$@"
 
 # 如果参数中有除 46b:e 之外的参数，则传递给 docker-compose
 if [ $# -gt 0 ]; then
@@ -28,16 +27,20 @@ fi
 
 case "$MORE_ARGS" in
 adguardhome)
+    bash ./adguardhome.sh
     $DOCKER_COMPOSE exec adguardhome /opt/adguardhome/AdGuardHome -s reload ||
         ($DOCKER_COMPOSE exec adguardhome true && $DOCKER_COMPOSE restart adguardhome) ||
         ($DOCKER_COMPOSE pull adguardhome && $DOCKER_COMPOSE down adguardhome && $DOCKER_COMPOSE up adguardhome -d)
     ;;
 nginx)
+    bash ./nginx.sh $RAW_ARGS
     ($DOCKER_COMPOSE exec nginx nginx -t && $DOCKER_COMPOSE exec nginx nginx -s reload) ||
         ($DOCKER_COMPOSE exec nginx nginx true && $DOCKER_COMPOSE restart nginx) ||
         ($DOCKER_COMPOSE pull nginx && $DOCKER_COMPOSE down nginx && $DOCKER_COMPOSE up nginx -d)
     ;;
 "")
+    bash ./nginx.sh $RAW_ARGS
+    bash ./adguardhome.sh
     ($DOCKER_COMPOSE exec nginx nginx -t && $DOCKER_COMPOSE exec nginx nginx -s reload) ||
         ($DOCKER_COMPOSE exec nginx nginx true && $DOCKER_COMPOSE restart nginx) ||
         ($DOCKER_COMPOSE pull nginx && $DOCKER_COMPOSE down nginx && $DOCKER_COMPOSE up nginx -d)
